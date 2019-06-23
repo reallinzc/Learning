e4e4c3933e16690d9c03640ac64a26e2a0a71b50
reallinzc <40253842+reallinzc@users.noreply.github.com>
2019-06-23 12:52:53 +0800

    Update Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,7 +1,8 @@
 let body = $response.body
+let url = $reponse.url
 body = JSON.parse(body)
 body.advertisement_num = 0;
 body.advertisement_info = [];
 body = JSON.stringify(body);
-$notification.post(body);
+$notification.post(url);
 $done({body})
