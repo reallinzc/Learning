4e174b95542e2031c2da3ec17ca1bcb5f67d35a1
reallinzc <40253842+reallinzc@users.noreply.github.com>
2019-06-23 12:23:39 +0800

    Update Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,5 +1,2 @@
-var body = $response.body;
-let obj = JSON.parse(body);
-obj.packageName = "asdfdasd";
-body = JSON.stringify(obj);
-$done({body});
+let url = $reponse.url
+$notification.post('url', $network.dns.join(', '));
