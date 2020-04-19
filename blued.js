08eb73376db6fed872c38ecf71fb6504d4079f99
reallinzc <40253842+reallinzc@users.noreply.github.com>
2020-04-20 01:59:02 +0800

    Update blued.js

--- blued.js
+++ blued.js
@@ -1,9 +1,14 @@
-var url =$request.url;
-let headers = $request.headers;
-async function launch (){
-    if(headers['User-Agent'].indexOf("Blued")!=-1){
-        $notification.post('捕获到闪照','点击URL',url);
-    }
-    $done();
-}
-launch()
+#!name=Blued
+#!desc=get photo and remove ads
+#!system=ios
+
+[Rule]
+URL-REGEX,^http:\/\/www\.bldimg\.com\/splash\/,REJECT
+
+
+[MITM]
+hostname = %APPEND% *.bldimg.com
+
+[Script]
+# blued
+blued.js = script-path=https://raw.githubusercontent.com/reallinzc/Learning/master/blued.js,pattern=https:\/\/burn-chatfiles\.bldimg\.com\/\d{4}\/,type=http-request
