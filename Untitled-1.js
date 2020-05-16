3489de2a2b0fe6b4f174f54d31f16d67a349332f
林泽超 <reallinzc@gmail.com>
2020-05-17 02:12:04 +0800

    del

--- Untitled-1.js
+++
@@ -1,8 +0,0 @@
-let body = $response.body
-let url = $response.url
-body = JSON.parse(body)
-body.advertisement_num = 0;
-body.advertisement_info = [];
-body = JSON.stringify(body);
-$notification.post(url);
-$done({body})
