6407e1f7d7d834c902c3af2353f2d9120c92e102
林泽超 <reallinzc@gmail.com>
2020-09-11 12:58:56 +0800

    适配新版本

--- JS/blued.js
+++ JS/blued.js
@@ -2,7 +2,7 @@ var url =$request.url;
 let headers = $request.headers;
 async function launch (){
     if(headers['User-Agent'].indexOf("Blued")!=-1){
-        $notification.post('捕获到闪照','下拉点击URL不要在app按住查看',url);
+        $notification.post('捕获到闪照','下拉点击URL不要在app按住查看','点击',{url: url});
     }
     $done();
 }
